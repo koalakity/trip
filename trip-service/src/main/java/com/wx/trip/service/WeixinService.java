c4dff648cc6565d3e80f8086c916f1deab14051b
wangtengfei <wangtengfei@jinlufund.com>
2016-04-28 11:47:42 +0800

    添加模板文件

--- trip-service/src/main/java/com/wx/trip/service/WeixinService.java
+++ trip-service/src/main/java/com/wx/trip/service/WeixinService.java
@@ -1,8 +1,10 @@
 package com.wx.trip.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class WeixinService {
 
 }
